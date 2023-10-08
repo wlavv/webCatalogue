9c820bf4a1d0e48747006ecd4591b8e8426e99db
Bruno <wl_avv@hotmail.com>
2023-10-09 00:03:26 +0100

    Initial setup Laravel
    Initial setup Laravel PHP 8.2

--- routes/web.php
+++ routes/web.php
@@ -8,28 +8,11 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
 |
 */
 
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/mindar', function () {
-    return view('mindar');
-});
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
