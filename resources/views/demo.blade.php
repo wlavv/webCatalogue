11e4b51fe19c8d10c6ef1d34d5cc220c67856f7f
Bruno <wl_avv@hotmail.com>
2023-12-13 16:07:03 +0000

    Update demo.blade.php

--- resources/views/demo.blade.php
+++ resources/views/demo.blade.php
@@ -222,9 +222,8 @@
     
 <div class="container"><div class="flex-center position-ref full-height">
          <div class="content">
-            <div class="title m-b-md">MindAR</div>
             <div class="links">
-               <div class="example-container">
+               <div class="example-container" style="display: contents">
                   <div id="example-scanning-overlay" class="hidden">
                      <div class="inner">
                         <img src="https://cdn.jsdelivr.net/gh/hiukim/mind-ar-js@1.1.5/examples/image-tracking/assets/card-example/card.png">
